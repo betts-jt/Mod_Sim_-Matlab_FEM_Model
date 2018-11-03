8ea8a6ad52a2858fc72e6e8c63eea6bc478c73ca
betts-jt <44404102+betts-jt@users.noreply.github.com>
2018-11-03 15:33:48 +0000

    Generate the function to calcualte the 2x2 local element matrix

---
+++ localElementMatrix_Calculator.m
@@ -0,0 +1,11 @@
+function [mesh] = localElementMatrix_Calculator(D, En, Mesh)
+%This fuction calculates the local 2-by-2 element matrix for the diffusion 
+%operator, which can calculate this matrix for an arbitrary element eN 
+%defined between the points x0 and x1
+    %%Where D is the diffusion coefficient, En is the local element number
+    %%and Mesh is the mesh data structure calcualted using
+    %%OneDimLinearMeshGen. From this mesh each elements Jacobian and the
+    %%nodal posisions x0 and x1 can be accesed.
+
+    
+end
