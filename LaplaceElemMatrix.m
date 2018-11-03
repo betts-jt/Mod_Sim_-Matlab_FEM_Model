08334d408f92b6630c988031aaf70c674f7c3579
betts-jt <44404102+betts-jt@users.noreply.github.com>
2018-11-03 15:37:52 +0000

    Change the name of localElementMarix_Calculator to reflect the naming convention in CourseworkOneUnitTest

--- LaplaceElemMatrix.m
+++ LaplaceElemMatrix.m
@@ -1,4 +1,4 @@
-function [mesh] = localElementMatrix_Calculator(D, En, Mesh)
+function [mesh] = LaplaceElemMatrix(D, En, Mesh)
 %This fuction calculates the local 2-by-2 element matrix for the diffusion 
 %operator, which can calculate this matrix for an arbitrary element eN 
 %defined between the points x0 and x1
@@ -6,6 +6,9 @@ function [mesh] = localElementMatrix_Calculator(D, En, Mesh)
     %%and Mesh is the mesh data structure calcualted using
     %%OneDimLinearMeshGen. From this mesh each elements Jacobian and the
     %%nodal posisions x0 and x1 can be accesed.
+
+% Run this Mesh = OneDimLinearMeshGen(0,1,3); to generate test mesh
+
 
     
 end
