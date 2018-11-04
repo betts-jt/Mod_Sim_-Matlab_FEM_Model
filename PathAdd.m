2cbb05757e0b92a211749a45c9d261a32d8ee464
betts-jt <01betjos@gmail.com>
2018-11-04 17:44:37 +0000

    add function to add all subfolders to the matlab path

---
+++ PathAdd.m
@@ -0,0 +1,10 @@
+function [] = PathAdd()
+% This code adds the relevent subfolders to the matlab path to enable the
+% code in these to be ran from the main folder
+
+addpath('Lib');
+addapth('Code');
+
+end
+
+
