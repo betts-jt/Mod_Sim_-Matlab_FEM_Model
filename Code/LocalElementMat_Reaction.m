a400b2bb8cd939d1c568936f39c0993e93cbc23a
betts-jt <01betjos@gmail.com>
2018-11-05 16:25:28 +0000

    update function description to include correct variable names

--- Code/LocalElementMat_Reaction.m
+++ Code/LocalElementMat_Reaction.m
@@ -1,7 +1,14 @@
-function [outputArg1,outputArg2] = LocalElementMat_Reaction(inputArg1,inputArg2)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [LocalElementMat_Reaction] = LocalElementMat_Reaction(llambda, eID, msh)
+%This fuction calculates the local 2-by-2 element matrix for the reaction 
+%operator, which can calculate this matrix for an arbitrary element eN 
+%defined between the points x0 and x1
+    %%Where llambda is a scalar coefficient, En is the local element number
+    %%and msh is the mesh data structure calcualted using
+    %%OneDimLinearMeshGen. From this mesh each elements Jacobian and the
+    %%nodal posisions x0 and x1 can be accesed.
+
+% Run this msh = OneDimLinearMeshGen(0,1,3); to generate test mesh
+
+
 end
 
