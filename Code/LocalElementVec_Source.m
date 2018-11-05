5864e646784fdfb7f569327f3109910f8e793303
betts-jt <01betjos@gmail.com>
2018-11-05 17:47:29 +0000

    add code to generate the final local source vector

--- Code/LocalElementVec_Source.m
+++ Code/LocalElementVec_Source.m
@@ -5,7 +5,11 @@ function [LocalVec_Source] = LocalElementVec_Source(f, eID, msh)
 %   msh is the mesh data structure calcualted using OneDimLinearMeshGen
 %   f = constant of the source term
 
+% Run this msh = OneDimLinearMeshGen(0,1,3); to generate test mesh
 
+J = msh.elem(eID).J; % rawing in the Jacobian of the element being analysed
+
+LocalVec_Source = [f*J f*J]; % Form the local source vector. The derrivation of this can be found in the corsework report part 1c.
 
 end
 
