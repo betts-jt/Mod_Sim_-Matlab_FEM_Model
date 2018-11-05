72aee754f9f4309ec384a998bc259be6f6b50f57
betts-jt <01betjos@gmail.com>
2018-11-05 17:43:29 +0000

    Generate code to form the local source vector

---
+++ Code/LocalElementVec_Source.m
@@ -0,0 +1,11 @@
+function [LocalVec_Source] = LocalElementVec_Source(f, eID, msh)
+%This code will generate the local element vector from the source term for
+%a given element number.
+%   Where eID is the element number.
+%   msh is the mesh data structure calcualted using OneDimLinearMeshGen
+%   f = constant of the source term
+
+
+
+end
+
