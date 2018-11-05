fb03fd6b3b11216c4aa22d6349dcbc07f5ecf5ff
betts-jt <01betjos@gmail.com>
2018-11-05 16:22:05 +0000

    add test to check matrix relative magnitudes

--- Code/Part1bUnitTest.m
+++ Code/Part1bUnitTest.m
@@ -1,11 +1,11 @@
 %% Test 1: test symmetry of the matrix
 % Test that this matrix is symmetric
 tol = 1e-14;
-D = 2; %diffusion coefficient
+lambda = 1; % scalar coefficient 
 eID=1; %element ID
 msh = OneDimLinearMeshGen(0,1,10);
 
-elemat = LocalElementMat_Reaction(D,eID,msh); 
+elemat = LocalElementMat_Reaction(lambda,eID,msh); 
 
 assert(abs(elemat(1,2) - elemat(2,1)) <= tol)
 
@@ -13,15 +13,15 @@ assert(abs(elemat(1,2) - elemat(2,1)) <= tol)
 % % Test that for two elements of an equispaced mesh, as described in the
 % % lectures, the element matrices calculated are the same
 tol = 1e-14;
-D = 5; %diffusion coefficient
+lambda = 1; % scalar coefficient 
 eID=1; %element ID
 msh = OneDimLinearMeshGen(0,1,10);
 
-elemat1 = LocalElementMat_Reaction(D,eID,msh);%THIS IS THE FUNCTION YOU MUST WRITE
+elemat1 = LocalElementMat_Reaction(lambda,eID,msh);
 
 eID=2; %element ID
 
-elemat2 = LocalElementMat_Reaction(D,eID,msh);%THIS IS THE FUNCTION YOU MUST WRITE
+elemat2 = LocalElementMat_Reaction(lambda,eID,msh);
 
 diff = elemat1 - elemat2;
 diffnorm = sum(sum(diff.*diff));
@@ -31,14 +31,25 @@ assert(abs(diffnorm) <= tol)
 % % Test that element 1 of the three element mesh problem described in the lectures
 % % the element matrix is evaluated correctly
 tol = 1e-14;
-D = 1; %diffusion coefficient
 eID=1; %element ID
+lambda = 1; % scalar coefficient 
 msh = OneDimLinearMeshGen(0,1,3);
-lambda = 1;
 
-elemat1 = LocalElementMat_Reaction(D,lambda,eID,msh);%THIS IS THE FUNCTION YOU MUST WRITE
+elemat1 = LocalElementMat_Reaction(lambda,eID,msh);
 
 elemat2 = [ 1/9 1/18; 1/18 1/9];
 diff = elemat1 - elemat2; %calculate the difference between the two matrices
 diffnorm = sum(sum(diff.*diff)); %calculates the total squared error between the matrices
 assert(abs(diffnorm) <= tol)
+
+%% Test 4: Test that the elements of the matrix are of the correct magnitude in relating to eachother.
+% test that elements 1 and 4 are double that of elements 2 and 3.
+tol = 1e-14;
+eID=1; %element ID
+lambda = 1; % scalar coefficient 
+msh = OneDimLinearMeshGen(0,1,3);
+
+elemat = LocalElementMat_Reaction(lambda,eID,msh);
+
+assert(abs(elemat(1) - 2*elemat(3) <= tol))
+assert(abs(elemat(4) - 2*elemat(2) <= tol))
