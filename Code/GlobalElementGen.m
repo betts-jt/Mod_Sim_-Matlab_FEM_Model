ece0778394f6a9cba07fc32e5c4bddc7ea0b34de
betts-jt <01betjos@gmail.com>
2018-11-05 17:38:54 +0000

    add code to construct the global element matrix

--- Code/GlobalElementGen.m
+++ Code/GlobalElementGen.m
@@ -1,4 +1,4 @@
-function [] = GlobalElementGen(xmin, xmax, Ne, D, llambda, reactionNeeded)
+function [Global_Mat] = GlobalElementGen(xmin, xmax, Ne, D, llambda, reactionNeeded)
 % Given the relevent input paramenters this cod ewill generate the local
 % element matracies for each element and combign these to form the global
 % element matrix for both the diffution operator and the reaction operator
@@ -17,13 +17,26 @@ function [] = GlobalElementGen(xmin, xmax, Ne, D, llambda, reactionNeeded)
 
 msh = OneDimLinearMeshGen(xmin,xmax,Ne); % Generate the mesh
 
+% GENERATE LOCAL ELEMENT MATRACIERS FOR ALL ELEMENTS
 for i = 1:Ne
-    DiffusionMat(i) = LaplaceElemMatrix(D, i, msh); % Generate the local element diffution matrix for element i 
+    Diffusion(i).Local = LaplaceElemMatrix(D, i, msh); % Generate the local element diffution matrix for element i
     
     if reactionNeeded == 1 % Check if the reaction matrix is required
-        ReactionMat(i) = LocalElementMat_Reaction(llambda, eID, msh); % Generate the local element reaction matrix for element i 
+        Reaction(i).Local = LocalElementMat_Reaction(llambda, i, msh); % Generate the local element reaction matrix for element i
+        Overall(i).Local = Diffusion(i).Local - Reaction(i).Local;% Calculate the overall local element matrix of the left hand side of the equation if the reaction term is needed
     else
+        Overall(i).Local = Diffusion(i).Local; % Calculate the overall local element matrix of the left hand side of the equation if the reaction term isn't needed
     end
-    
+end
+
+Global_Mat = zeros(Ne+1); % Generate blank global matrix for population.
+
+% GENREATE THE GLOBAL MATRIX
+for i = 1:Ne
+    Global_Mat(i:i+1,i:i+1) =  Global_Mat(i:i+1,i:i+1)+Overall(i).Local;
+end
+
+4
+
 end
 
