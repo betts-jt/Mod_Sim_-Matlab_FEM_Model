37a308abd1b0bf9c3e1202f25d6669822854a886
betts-jt <01betjos@gmail.com>
2018-11-05 22:05:26 +0000

    generate a function to complete part 1c

---
+++ Part1c.m
@@ -0,0 +1,13 @@
+function [] = Part1c()
+% This code runs the FEM_BC FEM solver for two example cases and compares
+% the results given to the analytical results for analysis.
+
+
+
+
+C1 = FEM_BC(1,2,1,0);
+
+
+
+end
+
