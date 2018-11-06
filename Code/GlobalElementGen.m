f35c4d662dcdcd36b8d7afa72b13885a61e97376
betts-jt <01betjos@gmail.com>
2018-11-06 23:22:01 +0000

    add code to allow the source function to be variable

--- Code/GlobalElementGen.m
+++ Code/GlobalElementGen.m
@@ -3,30 +3,29 @@ function [Global_Mat, SourceGlobal_Vec] = GlobalElementGen(xmin, xmax, Ne, D, ll
 % element matracies for each element and combign these to form the global
 % element matrix for both the diffution operator and the reaction operator
 % if needed.
-%   Where:
-%   xmin = the mimimum value of x to be used when generating the mesh.
-%   xmax = the maxmum value of x to be used when generating the mesh.
-%   Ne = Number of elements to be used when generating the mesh.
-%   D = Diffution operator used when generating the local element matracies
+%   Where: xmin = the mimimum value of x to be used when generating the
+%   mesh. xmax = the maxmum value of x to be used when generating the mesh.
+%   Ne = Number of elements to be used when generating the mesh. D =
+%   Diffution operator used when generating the local element matracies
 %       for diffution.
 %   llambda = Scalar coefficient used when generating the lcoal element
 %             matracies for reaciton operator.
-%   f = spacial coefficient for calculating the source term
-%   ReactionNeeded = This is either 1 is the global element matrix for the
-%                   reaction operator needs to be generatod or 0 if it does not need to be
-%                   gerenated.
+%   f = spacial coefficient for calculating the source term ReactionNeeded
+%   = This is either 1 is the global element matrix for the
+%                   reaction operator needs to be generatod or 0 if it does
+%                   not need to be gerenated.
 %   SourceTermConstant = 1 if the source term is constant and 0 if the
 %                        source term is not fixed
 
 msh = OneDimLinearMeshGen(xmin,xmax,Ne); % Generate the mesh
-
+SourceTermConstant
 % GENERATE LOCAL ELEMENT MATRACIERS FOR ALL ELEMENTS
 for i = 1:Ne
     Diffusion(i).Local = LaplaceElemMatrix(D, i, msh); % Generate the local element diffution matrix for element i
-    if SourceTermConstant = 1 % check if the source term is constnat
+    if SourceTermConstant == 1 % check if the source term is constnat
         Source(i).Local = LocalElementVec_Source(f, i, msh); % Generate the local element source vector for element i
-    elseif SourceTermConstant = 0 % Check if the sourceterm is not constant
-        Source(i).Local = LocalElementVec_Source(f, i, msh); % Generate the local element source vector for element i
+    elseif SourceTermConstant == 0 % Check if the sourceterm is not constant
+        Source(i).Local = LocalElementVec_VariedSource(f, i, msh); % Generate the local element source vector for element i
     else
         error('Please enter either a value of 0 ro 1 for SourceTermConstant')
     end
