fe6e004fba8d0c6f9a827b5c2d0ea803d8c3d0c5
betts-jt <01betjos@gmail.com>
2018-11-06 23:55:47 +0000

    add x1 to second local vector element closes #1

--- Code/LocalElementVec_VariedSource.m
+++ Code/LocalElementVec_VariedSource.m
@@ -13,7 +13,7 @@ J = msh.elem(eID).J; % rawing in the Jacobian of the element being analysed
 x0 = msh.elem(eID).x(1);
 x1 = msh.elem(eID).x(2);
 
-LocalVec_Source = [(f*J*(1+(8/3)*x0+(4/3)*x1)) (f*J*(1+(8/3)*x0+(4/3)))]; % Form the local source vector. The derrivation of this can be found in the corsework report part 1c.
+LocalVec_Source = [(f*J*(1+(8/3)*x0+(4/3)*x1)) (f*J*(1+(8/3)*x0+(4/3)*x1))]; % Form the local source vector. The derrivation of this can be found in the corsework report part 1c.
 
 
 end
