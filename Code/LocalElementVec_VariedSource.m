a952745e09080bb84de60a30ba158074075a2a1d
betts-jt <01betjos@gmail.com>
2018-11-06 23:21:35 +0000

    Chang ethe lcoal source vector term to be varied

--- Code/LocalElementVec_VariedSource.m
+++ Code/LocalElementVec_VariedSource.m
@@ -1,4 +1,4 @@
-function [LocalVec_Source] = LocalElementVec_Source(f, eID, msh)
+function [LocalVec_Source] = LocalElementVec_VariedSource(f, eID, msh)
 %This code will generate the local element vector from the source term for
 %a given element number.
 %   Where eID is the element number.
@@ -7,9 +7,13 @@ function [LocalVec_Source] = LocalElementVec_Source(f, eID, msh)
 
 % Run this msh = OneDimLinearMeshGen(0,1,3); to generate test mesh
 
+
 J = msh.elem(eID).J; % rawing in the Jacobian of the element being analysed
 
-LocalVec_Source = [f*J f*J]; % Form the local source vector. The derrivation of this can be found in the corsework report part 1c.
+x0 = msh.elem(eID).x(1);
+x1 = msh.elem(eID).x(1);
+
+LocalVec_Source = [f*J*(1+(8/3)*x0+(4/3)*x1) f*J*(1+(8/3)*x0+(4/3)]; % Form the local source vector. The derrivation of this can be found in the corsework report part 1c.
 
 
 end
