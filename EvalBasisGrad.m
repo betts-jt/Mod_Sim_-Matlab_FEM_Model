990617c8565d9dc065cc9751698221219933be74
betts-jt <01betjos@gmail.com>
2018-11-27 14:35:16 +0000

    edit variable names for consistancy

--- EvalBasisGrad.m
+++ EvalBasisGrad.m
@@ -1,10 +1,9 @@
-function [ dpsidxi ] = EvalBasisGrad(lnid)
+function [ dpsidxi ] = EvalBasisGrad(lnid, xi)
 %EvalBasisGrad Returns gradient of basis functions
 % Returns the gradients of the linear Lagrange basis functions for a
 % specificed local node id (lnid = 0 or 1) and xipt (gradient is a constant
 % value in this case, but for higher order basis functions, would vary
-% with xi - and hence the argument is included for generality/future
-proofing)
+% with xi - and hence the argument is included for generality/future proofing)
 %Use the node id to generate the sign of the basis gradient - ie.
 %either + or -. when lnid=0, sign is -ve, when lnid=1, sign is +ve.
 
