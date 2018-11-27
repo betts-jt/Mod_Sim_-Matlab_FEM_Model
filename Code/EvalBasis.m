73a0a4d930955d4fccd54784f7bfad2853a59864
betts-jt <44404102+betts-jt@users.noreply.github.com>
2018-11-27 15:08:11 +0000

    change variable names

--- Code/EvalBasis.m
+++ Code/EvalBasis.m
@@ -1,10 +1,10 @@
-function [ psi ] = EvalBasis(lnid,xi)
+function [ psi ] = EvalBasis(lnid,xipts)
 %EvalBasis Evaluates linear Lagrange basis functions
 % Given the local node id (lnid) for a linear Lagrange element, and a xi
 % coordinate (between [-1,1]), returns the correpsonding value of the
 % basis function for that local node, at that xi point
 
 sign = (-1)^(lnid+1);
-psi = 0.5*(1 + ((sign*xi))); %Basis Function at a given gauss point
+psi = 0.5*(1 + ((sign*xipts))); %Basis Function at a given gauss point
 
 end
--- Code/EvalField.m
+++ Code/EvalField.m
@@ -1,13 +1,14 @@
-function [ val ] = EvalField(msh,field,eID,xi)
+function [ val ] = EvalField(msh,field,eID,xipts)
 %EvalField Evaluates a nodally stored scalar field at a given xi point in an element
 % Evaluates a scalar data field (stored in field) that is represented on a linear Lagrange
 % finite element mesh (msh), when given a xi coordinate and an element id
 
-psi = [EvalBasis(0,xi) EvalBasis(1,xi)]; % Get vector containing basis function values at xipt
+psi = [EvalBasis(0,xipts) EvalBasis(1,xipts)]; % Get vector containing basis function values at xipt
 
-dpsidxi = [EvalBasis(0,xi) EvalBasis(1,xi)]; % Get vector containing basis function gradients at xipt
+dpsidxi = [EvalBasis(0,xipts) EvalBasis(1,xipts)]; % Get vector containing basis function gradients at xipt
 
 vcoeff = [field(msh.elem(eID).n(1)); field(msh.elem(eID).n(2))];
 %Get nodal values for element eID
+
 val = psi*vcoeff; %Dot product the two together to sum up the coefficients*basis functions to give the final interpolated value
 end
