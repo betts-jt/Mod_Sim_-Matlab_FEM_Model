d9a439ac72c423e7970022fdaa57cae38d75f791
betts-jt <01betjos@gmail.com>
2018-11-29 12:56:17 +0000

    Add additional code

--- Code/Part1.m
+++ Code/Part1.m
@@ -7,11 +7,11 @@ Data.Ne = 10; % Numeber of elements in the mesh
 Data. reactionNeeded = 0; % Value is either 1 is the problem needs the local element matracies due to reaction need to be calcualted or 0 is these are not needed
 Data.SourceNeeded = 0; % Value is either 1 is the problem needs the local element vector due to source need to be calcualted or 0 is these are not needed.
 Data.SourceTermConstant = 1; % Value defining whether the source term is constant
-
 Data.dt = 0.1; % Timestep for transient responce
 total_t = 5; % Total time for analysis
 N = total_t/Data.dt; % Number of timesteps
 
+Data.VariedParamaters = 0; % Value is either 1 if the equation parameters vary with x or 0 if they dont
 Data.Theta = 0; % Value difining the method to be used to solve the transient resonce. 0.5 for Crank Nicolson. 1 for Backward Euler
 
 
@@ -19,26 +19,30 @@ BC1T = 'D'; % Define type of BC 1
 BC1V = 0; % Value of BC1
 BC2T = 'D'; % Define type of BC 2
 BC2V = 1; % Value of BC2
+
 InitialCon = 0; % Initial condition of the problem in time
 
 time  = 0:Data.dt:(total_t); % Calculte the time for each timestep
-x = Data.xmin: Data
+x = Data.xmin: Data;
 
-c_results = zeros(N,Data.Ne)
-c_results(1,:) = InitialCon; 
+c_current = InitialCon;
+c_results = zeros(N,Data.Ne);
+c_results(1,:) = c_current; 
 
 for k  = 2:N+1
-    [c(k).time, Global_Mat(k).time, Global_Mat_K(k).time, Global_Mat_M(k).time, SourceGlobal_Vec(k).time] = Trans_FEM_BC(BC1T,BC1V,BC2T,BC2V, Data);
+    [c_next, Global_Mat_next, Global_Mat_K_next, Global_Mat_M_next, SourceGlobal_Vec_next] = Trans_FEM_BC(BC1T,BC1V,BC2T,BC2V, Data);
     
     % Calculate the matrix to multiply to the previous solution
-    A = Global_Mat_M(k).time-((1-Data.Theta)*Data.dt*Global_Mat_K(k).time);
+    A = Global_Mat_M_next-((1-Data.Theta)*Data.dt*Global_Mat_K_next);
     
     % Multiply the above by the previous solution
-    Ac = A*c(k-1).time;
+    Ac = A*c_current;
     
-    B = Data.dt*Data.Theta(SourceGlobal_Vec(k));
+    B = Data.dt*Data.Theta(SourceGlobal_Vec_next);
     
-    C = Data.dt*(1-Data.Theta)(SourceGlobal_Vec(k-1))
+    C = Data.dt*(1-Data.Theta)(SourceGlobal_Vec_current)
 
     Global_Vector = Ac + B + C; % Caluclate the global source vector
+    
+    
 end
