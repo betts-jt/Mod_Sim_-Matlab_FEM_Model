5959c2f6f789566801a891aa8fbd31229a8c886f
betts-jt <01betjos@gmail.com>
2018-11-29 17:07:23 +0000

    Generate the code to change the skin variables depending on position thorugh the skin

--- Code/EquationConstants.m
+++ Code/EquationConstants.m
@@ -2,4 +2,40 @@ function [Data] = EquationConstants(nvec, Data)
 %   This funciton computes the values of lambda, f and D where they vary
 %   across the x value of the material
 
+% SETTING THE THINCKNESSES OF THE SKIN
+E = 0.00166667; % Epidermis outer thickness
+D = 0.0005; % Dermis outer thickness
+B = 0.01; % Total skin thickness
+
+% Setting the variables that make up D, lambda and f depending on position
+% of the point through the skin
+
+if nvec <= E % If the point is in the Epidermis
+    k = 25; 
+    G = 0; 
+    rho = 1200; 
+    c = 3300;
+elseif nvec > E && nvec <= D % If the point is in the Epidermis
+    k = 40;
+    G = 0;
+    rho = 1200;
+    c = 3300;
+    rho_b = 1060;
+    c_b = 3770;
+    T_b = 310.15;
+elseif nvec >D && nvec <= B
+    k = 20;
+    G = 0; 
+    rho = 1200;
+    c = 3300; 
+    rho_b = 1060;
+    c_b = 3770;
+    T_b = 310.15;
+end
+
+Data.D = k/(rho*c); % Calculate the diffution coefficient
+Data.lambda = (G*rho_b*c_b)/(rho*c); % Calucalteing the reaction coefficient
+Data.f = (G*rho_b*c_b)/(rho*c)*T_b; % Calculating the source term
+end
+
 
