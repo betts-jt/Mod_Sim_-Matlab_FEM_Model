f2c5f2e64bf69326a0c65f93ca775940b831b4de
betts-jt <01betjos@gmail.com>
2018-11-29 18:53:09 +0000

    generate function to compute tissue damage

---
+++ TissueDamage.m
@@ -0,0 +1,10 @@
+function [] = TissueDamage(Data)
+%   This function computes the level of tissue damage on the skin given a
+%   tempurature input on the skins surface. In addition to this it
+%   indicates whether a second degree burn has taken place.
+
+
+
+
+
+end
