42915cd6802c62fa75d581c5e68aabf5576fc640
betts-jt <01betjos@gmail.com>
2018-11-30 13:37:50 +0000

    add further optimisation reducting timestep as we get closer to solution

--- Coursework2_answer_functions/Part2_1b_Optimiser.m
+++ Coursework2_answer_functions/Part2_1b_Optimiser.m
@@ -1,7 +1,17 @@
 N = 10; % Numebr of loops to be done
-SkinTemp = [310:(390-310)/(N-1):390];
 
-parfor i = 1:N
-    i
-    [gamma(i), ~]=Part2_1b(SkinTemp(i), 1);
+TempMax = 390;
+TempMin = 310;
+timeSteps = [0.5, 0.01, 0.005];
+
+for k = 1:length(timeSteps)
+    SkinTemp = [TempMin:(TempMax-TempMin)/(N-1):TempMax]
+    
+    parfor i = 1:N
+        i
+        [gamma(i), ~]=Part2_1b(SkinTemp(i), 1, timeSteps(k));
+    end
+    
+    TempMin = SkinTemp(max(find(gamma<1)));
+    Tempmax = SkinTemp(min(find(gamma>1)));
 end
