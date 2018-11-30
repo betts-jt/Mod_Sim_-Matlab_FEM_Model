a279b02cd05dec91af5b33d60a7ae38f0d5c694e
betts-jt <01betjos@gmail.com>
2018-11-30 21:04:10 +0000

    off laod FEM solving to transient solver function

--- Coursework2_answer_functions/Part2_1b.m
+++ Coursework2_answer_functions/Part2_1b.m
@@ -14,14 +14,18 @@ Data.Ne = 600; % Numeber of elements in the mesh
 Data. reactionNeeded = 1; % Value is either 1 is the problem needs the local element matracies due to reaction need to be calcualted or 0 is these are not needed
 Data.SourceTermConstant = 1; % Value defining whether the source term is constant
 Data.dt = timeStep; % Timestep for transient responce
-total_t = 10; % Total time for analysis
-N = total_t/Data.dt; % Number of timesteps
-
 Data.VariedParamaters = 1; % Value is either 1 if the equation parameters vary with x or 0 if they dont
+Data.GN = 2; % N value for gausian quadriture
+Data.optimise = optimise; % Add optimise value ot data structurre
+
+total_t = 10; % Total time for analysis
+Data.N = total_t/Data.dt; % Number of timesteps
+Data.time  = 0:Data.dt:(total_t); % Calculte the time for each timestep
+Data.x = Data.xmin: (Data.xmax-Data.xmin)/Data.Ne:Data.xmax;
 
 % SELECT SOLVING METHOD
 % Check if the code if running once or though an optimiser
-if optimise == 0 % No optimisation in taking place
+if Data.optimise == 0 % No optimisation in taking place
     % Allow used to select solving method
     answer = questdlg('Select a sovling method','Solving Method Choice', 'Crank-Nicolson', 'Backwards Euler','Backwards Euler');
     % Handle response
@@ -35,16 +39,11 @@ if optimise == 0 % No optimisation in taking place
             disp([answer '  is the selected solving method'])
             Data.Theta = 1;
     end
-elseif optimise ==1 % Optimisation is taking place
+elseif Data.optimise ==1 % Optimisation is taking place
     Data.Theta = 1; % Set the solving method used in the optimiser
 else
     error('Enter either 0, or 1 for the variable optimise')
 end
-
-%SET N FOR GAUSSIAN QUADRITURE SCHEME
-Data.GN = 2;
-
-
 
 % SET SOLVING PERAMATERS
 if Data.VariedParamaters == 0
@@ -56,80 +55,18 @@ else
     error('Please enter either 0 or 1 for Data.VariedParamaters')
 end
 
-% INITIALISE DATA MESH
-msh = OneDimLinearMeshGen(Data.xmin,Data.xmax,Data.Ne); % Generate the mesh
 
 % SET UP BOUNDARY CONDITIONS
-BC1T = 'D'; % Define type of BC 1
-BC1V = SurfaceTemp; % Value of BC1
-BC2T = 'D'; % Define type of BC 2
-BC2V = 310.15; % Value of BC2
+Data.BC1T = 'D'; % Define type of BC 1
+Data.BC1V = SurfaceTemp; % Value of BC1
+Data.BC2T = 'D'; % Define type of BC 2
+Data.BC2V = 310.15; % Value of BC2
+Data.InitialCon = 310.15; % Initial condition of the problem in time
 
-InitialCon = 310.15; % Initial condition of the problem in time
+%RUN TRANSIENT FEM SOLVER
+[c_results, Data] = TransientFEMSolver_Part2(Data);
 
-time  = 0:Data.dt:(total_t); % Calculte the time for each timestep
-x = Data.xmin: (Data.xmax-Data.xmin)/Data.Ne:Data.xmax;
-
-c_current=zeros(Data.Ne+1, 1);
-c_current(:,1) = InitialCon;
-c_results = zeros(N,Data.Ne+1);
-c_results(1,:) = c_current;
-TempE(1) = InitialCon;
-
-% Apply BC to this initial solution (only aplies to Dirichlet)
-if BC1T == 'D'
-    c_current(1,1) = BC1V; % Set start value to the value of BC1
-end
-if BC2T == 'D'
-    c_current(end,1) = BC2V; % Set last value to that of BC2
-end
-
-% INITIALISE MATRACIES
-Global_Mat_K = zeros(Data.Ne+1);
-Global_Mat_M = zeros(Data.Ne+1);
-Global_Mat = zeros(Data.Ne+1);
-Global_Vec = zeros(Data.Ne+1, 1);
-SourceVec_current = zeros(Data.Ne+1,1);
-SourceVec_next = zeros(Data.Ne+1,1);
-
-for k  = 2:N+1
-    % CALCULATE THE GLOBAL MATRIX AND VECTOR
-    [Global_Mat, Global_Vec, SourceVec_next] = GlobalMat_GlobalVec_Assbemly(msh, c_current, Data, Global_Mat_K, Global_Mat_M, SourceVec_current, SourceVec_next);
-    
-    % APPLY BOUNDARY CONDITIONS
-    [Global_Mat, Global_Vec] = ApplyBC(BC1T,BC1V,BC2T,BC2V, Data, Global_Mat, Global_Vec);
-    
-    c_next = Global_Mat\Global_Vec; % generate the solution at the next point
-    
-    c_current = c_next; % set current to calue of c next
-    c_results(k,:) = c_current'; % Store c_current to file
-    
-    % FIND TEMPURATURE AT POINT E
-    [E_point] =find(round(msh.nvec, 9)==0.001666667); % find when x = E (0.00166667)
-    TempE(k) = c_results(k, E_point);
-    
-    SourceVec_current = SourceVec_next;
-    
-    % REINITIALISE MATRACIES
-    Global_Mat_K = zeros(Data.Ne+1);
-    Global_Mat_M = zeros(Data.Ne+1);
-    Global_Mat = zeros(Data.Ne+1);
-    Global_Vec = zeros(Data.Ne+1, 1);
-    SourceVec_next = zeros(Data.Ne+1,1);
-    
-    % Check if optimisation is taking place
-    if Data.optimise == 0 % Answer is not being optimised. Plot graphs
-        figure(1)
-        plot(x,c_results(k,:)')
-        ylabel('Tempurature, K')
-        xlabel('Distance through skin, mm')
-        legend(['Current Time ' num2str(time(k)) 's'], 'Location', 'NorthWest')
-    elseif Data.optimise ==1 % Answer is being optimised. Don't plot graphs
-    else
-        error('Enter either 0, or 1 for the variable optimise')
-    end
-    
-end
 % DETERNIME IF BURNING OCCURS/WHEN
-[Gamma BurningStart]= TissueDamage(Data, TempE, time);
+[Gamma, BurningStart]= TissueDamage(Data, Data.TempE, Data.time);
+
 end
