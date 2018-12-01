88a45c650f2568f7c28dd4b7508edc3dd14667f2
betts-jt <01betjos@gmail.com>
2018-12-01 13:04:20 +0000

    Update GlobalMat_GlobalVec_Assbemly.m

--- Code/GlobalMat_GlobalVec_Assbemly.m
+++ Code/GlobalMat_GlobalVec_Assbemly.m
@@ -1,11 +1,15 @@
-function [Global_Mat, Global_Vec, SourceVec_next] = GlobalMat_GlobalVec_Assbemly(msh, C_current, Data, Global_Mat_K, Global_Mat_M, SourceVec_Current, SourceVec_next)
+function [Global_Mat, Global_Vec, SourceVec] = GlobalMat_GlobalVec_Assbemly(msh, C_current, Data, Global_Mat_K, Global_Mat_M, SourceVec)
 % This is a fuction to generate the global matrix and global vector for a
 % give mesh and intiial conditions.
 %   msh = the mesh being investigated
 %   C_current = current colution value
 %   Data = Structure of probelem variables.
 
-
+SourceVecComponentCurent_GV = Data.dt*((1-Data.Theta).*SourceVec);
+SourceVec = zeros(Data.Ne+1,1);
+Global_Mat = zeros(Data.Ne+1);
+Global_Vec = zeros(Data.Ne+1, 1);
+    
 for i = 1:Data.Ne
     if Data.VariedParamaters == 1
         [Data] = EquationConstants(msh.nvec(i+1), Data);
@@ -27,12 +31,13 @@ for i = 1:Data.Ne
     % This correctly sums the overlapping values on the diagonal.
     Global_Mat_K(i:i+1,i:i+1) =  Global_Mat_K(i:i+1,i:i+1)+Stiffness_Local;
     Global_Mat_M(i:i+1,i:i+1) =  Global_Mat_M(i:i+1,i:i+1)+Mass_Local;
-    SourceVec_next(i:i+1,1) = SourceVec_next(i:i+1) + Source_Local_next';
+    SourceVec(i:i+1,1) = SourceVec(i:i+1) + Source_Local_next';
 end
 
-SourceVecComponent_GV = Data.dt*((Data.Theta.*SourceVec_next)+(1-Data.Theta).*SourceVec_Current);
+SourceVecComponentNext_GV = Data.dt*((Data.Theta.*SourceVec));
 
 Global_Mat = Global_Mat_M + (Data.Theta* Data.dt .* Global_Mat_K); % Caluclate the global matrix
-Global_Vec = ((Global_Mat_M - ((1-Data.Theta) * Data.dt * Global_Mat_K)) * C_current)+SourceVecComponent_GV; % Calculate the global vector
+Global_Vec_Initial = Global_Mat_M - ((1-Data.Theta) * Data.dt).* Global_Mat_K;
+Global_Vec =  Global_Vec_Initial*C_current + SourceVecComponentNext_GV+SourceVecComponentCurent_GV; % Calculate the global vector
 
 end
