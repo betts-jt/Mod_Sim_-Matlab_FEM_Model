fdf20215fd73ca1ecd70d83768918298e218f039
betts-jt <01betjos@gmail.com>
2018-12-02 16:07:00 +0000

    update formatting for report

---
+++ Code/SolvingMethod.m
@@ -0,0 +1,13 @@
+function [Data] = SolvingMethod(Data)
+
+% Open box to allow user to select solving method
+answer = questdlg('Select a sovling method','Solving Method Choice', 'Crank-Nicolson', 'Backwards Euler','Backwards Euler');
+% Handle response
+switch answer
+    case 'Crank-Nicolson'
+        disp([answer ' is the selected solving method'])
+        Data.Theta = 0.5; % Set theta to 0.05 if Crank-Nicolson is selected
+    case 'Backwards Euler'
+        disp([answer '  is the selected solving method'])
+        Data.Theta = 1; % Set theta to 0.05 if Crank-Nicolson is selected
+end
