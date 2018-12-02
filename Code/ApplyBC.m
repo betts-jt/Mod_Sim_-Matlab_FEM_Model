3c15747266757790e18f194e39b8a3945a61f7d0
betts-jt <01betjos@gmail.com>
2018-12-02 17:07:31 +0000

    Change formatting for report

--- Code/ApplyBC.m
+++ Code/ApplyBC.m
@@ -1,4 +1,5 @@
-function [Global_Mat, Global_Vec] = ApplyBC(BC1T,BC1V,BC2T,BC2V, Data, Global_Mat, Global_Vec)
+function [Global_Mat, Global_Vec] = ApplyBC(BC1T,BC1V,BC2T,BC2V, Data,...
+    Global_Mat, Global_Vec)
 % This function will apply  boundary conditions to a transient 1D FEM model
 %   Where;
 %   BC1T is the type of boundary condition 1 at the position x0. This will
@@ -23,22 +24,28 @@ function [Global_Mat, Global_Vec] = ApplyBC(BC1T,BC1V,BC2T,BC2V, Data, Global_Ma
 if BC1T == 'D' % Check if the first boundary is a Dirichlet boundary
     Global_Mat(1,:) = 0; % Set the top row of the global matrix to 0
     Global_Mat(1) = 1; % Set the top diagonal of the global matrix to 1
-    Global_Vec(1) = BC1V; % Set the top value of the global source vector to the input value of BC1
+    % Set the top value of the global source vector to the input value of BC1
+    Global_Vec(1) = BC1V; 
 elseif BC1T == 'N' % Check if the first boundary is a Neumann boundary
-    Global_Vec(1) = Global_Vec(1) + -BC1V; % add the value of -D(dc/dx)x=0 to the first element of the source term vector
+    % add the value of -D(dc/dx)x=0 to the first element of the source term vector
+    Global_Vec(1) = Global_Vec(1) + -BC1V; 
 else % If neither 1 or 2 are entered for BC1
-    error('Incorrect boundary condition type entered for BC1. Enter either D for a Dirichlet boundary condition or N for a Neumann boundary condition')
+    error(['Incorrect boundary condition type entered for BC1. Enter either'...
+        ' D for a Dirichlet boundary condition or N for a Neumann boundary condition'])
 end
 
 %BOUNDARY CONDITION 2
 if BC2T == 'D' % Check if the second boundary is a Dirichlet boundary
     Global_Mat(end,:) = 0; % Set the top row of the global matrix to 0
     Global_Mat(end) = 1; % Set the bottom diagonal of the global matrix to 1
-    Global_Vec(end) = BC2V; % Set the top value of the global source vector to the input value of BC1
+    % Set the top value of the global source vector to the input value of BC1
+    Global_Vec(end) = BC2V; 
 elseif BC2T == 'N' % Check if the second boundary is a Neumann boundary
-    Global_Vec(end) = BC2V; % add the value of D(dc/dx)x=1 to the last element of the source term vector
+    % add the value of D(dc/dx)x=1 to the last element of the source term vector
+    Global_Vec(end) = BC2V; 
 else % If neither 1 or 2 are entered for BC1
-    error('Incorrect boundary condition type entered for BC2. Enter either D for a Dirichlet boundary condition or N for a Neumann boundary condition')
+    error(['Incorrect boundary condition type entered for BC2. Enter either'...
+        'D for a Dirichlet boundary condition or N for a Neumann boundary condition'])
 end
 
 end
