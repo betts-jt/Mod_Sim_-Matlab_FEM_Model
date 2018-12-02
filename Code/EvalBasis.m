da8114b6d916520855ff8baeb6c8f4d3d69e8f9c
betts-jt <01betjos@gmail.com>
2018-12-02 02:42:36 +0000

    generate function to give basis functions at each node

--- Code/EvalBasis.m
+++ Code/EvalBasis.m
@@ -1,10 +1,15 @@
-function [ psi ] = EvalBasis(lnid,xipts)
+function [ Phi ] = EvalBasis(lnid,GP)
 %EvalBasis Evaluates linear Lagrange basis functions
-% Given the local node id (lnid) for a linear Lagrange element, and a xi
-% coordinate (between [-1,1]), returns the correpsonding value of the
-% basis function for that local node, at that xi point
+% Given the local node id (lnid) for a linear Lagrange element, and a gauss point (GP), returns the correpsonding value of the
+% basis function for that local node.
 
-sign = (-1)^(lnid+1);
-psi = 0.5*(1 + ((sign*xipts))); %Basis Function at a given gauss point
-
+% CHECK LOCAL NODE POINT AND ASSIGN BASIS GRADIENT AT THAT POINT
+if lnid == 1
+    Phi = (GP*(GP-1))/2;
+elseif lnid == 2
+    Phi = 1-GP^2;
+elseif lnid == 3
+    Phi = (GP*(GP+1))/2;
+end
+    
 end
