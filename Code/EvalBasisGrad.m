7acf1019aaf2030525b48e60b69b13d887e606bc
betts-jt <01betjos@gmail.com>
2018-12-02 02:43:08 +0000

    Edit function to calculate the differential of the basis at each node

--- Code/EvalBasisGrad.m
+++ Code/EvalBasisGrad.m
@@ -1,13 +1,20 @@
-function [ dpsidxi ] = EvalBasisGrad(lnid, xipts)
+function [ dPsidXi ] = EvalBasisGrad(lnid, GP)
 %EvalBasisGrad Returns gradient of basis functions
 % Returns the gradients of the linear Lagrange basis functions for a
-% specificed local node id (lnid = 0 or 1) and xipt (gradient is a constant
-% value in this case, but for higher order basis functions, would vary
-% with xi - and hence the argument is included for generality/future proofing)
-%Use the node id to generate the sign of the basis gradient - ie.
-%either + or -. when lnid=0, sign is -ve, when lnid=1, sign is +ve.
+% specificed local node id (lnid = 0,1 or 3) and gauss point GP. For this case the
+% differentials of tha basis gradient have been worked out by hand fomr the
+% basis functions;
+%   Phi(1) = (GP*(GP-1))/2;
+%   Phi(2) = 1-GP^2;
+%   Phi(3) = (GP*(GP+1))/2;
 
-sign = (-1)^(lnid+1);
-dpsidxi = 0.5 * sign; %Basis Function gradient at a given gauss point
-
+% CHECK LOCAL NODE POINT AND ASSIGN BASIS GRADIENT AT THAT POINT
+if lnid == 1
+    dPsidXi = GP-0.5;
+elseif lnid == 2
+    dPsidXi = -2*GP;
+elseif lnid == 3
+    dPsidXi = GP+0.5;
+end
+        
 end
