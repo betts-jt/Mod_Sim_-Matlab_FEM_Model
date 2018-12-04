394af12fb838873be72f949f274b0f81a749a395
betts-jt <01betjos@gmail.com>
2018-12-04 00:47:24 +0000

    add graph plot

--- Coursework2_answer_functions/Part2_Optimiser.m
+++ Coursework2_answer_functions/Part2_Optimiser.m
@@ -2,11 +2,14 @@ N = 10; % Numebr of loops to be done
 
 TempMax = 390; % Maximum tempurature to be tested initially
 TempMin = 310; % Minimum tempurature to be tested initially
-timeSteps = [0.05, 0.005, 0.0005, 0.0005]; % Array of timesteps to be tested. Can be any legnth
+% Array of timesteps to be tested. Can be any legnth
+timeSteps = [0.5, 0.5, 0.05, 0.05, 0.005]; 
 
+% Loop thourgh as many tiumes as timesteps there are in the timesteps array
 for k = 1:length(timeSteps)
     % Generate array of legnth N of tempuratures to be tried
     SkinTemp = [TempMin:(TempMax-TempMin)/(N-1):TempMax];
+    
     % DISPLAY CURRENT TESTING CONDITIONS
     disp('***********************************')
     disp(['Loop ' num2str(k) ' of ' num2str(length(timeSteps))])
@@ -18,12 +21,17 @@ for k = 1:length(timeSteps)
     parfor i = 1:N
         [gamma(i), ~]=Part2(SkinTemp(i), 1, timeSteps(k));
     end
-    
+
     %SET UP VARIABLES FOR NEXT LOOP
     TempMin = SkinTemp(max(find(gamma<1)));
     TempMax = SkinTemp(min(find(gamma>1)));
+    a(k) = abs(1-gamma(max(find(gamma<1))));
 end
 
+figure(1)
+plot([1:5], a)
+xlabel('optimisation loop')
+ylabel('1-\Gamma')
 % DISPLAYING OUTCOME OF OPTIMISER
 disp('***********************************')
 disp('***********************************')
@@ -41,6 +49,9 @@ if TempReduction > TempReduction_roundedDown + 0.5
 else 
     TempReduction = TempReduction_roundedDown+0.5; % If it is add 0.5 to rounded answer 
 end
-disp(['The required tempurature reduction of the protective clothing is ' num2str(TempReduction) 'K to the nearest 0.5 K'])
+% Display results in command window
+disp(['The required tempurature reduction of the protective clothing is '...
+    num2str(TempReduction) 'K to the nearest 0.5 K'])
 disp('***********************************')
 disp('***********************************')
+
